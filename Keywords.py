6e9c23aa622dbb1dfdfbc6e9b1fc5002b8e45871
EhsanGraph <122868704+EhsanGraph@users.noreply.github.com>
2023-06-09 11:26:25 +0200

    Update Keywords.py

--- Keywords.py
+++ Keywords.py
@@ -6,7 +6,7 @@ longtail = []
 
 
 session = requests_html.HTMLSession()
-req = session.get("https://www.google.com/search?q=Afghan+Nobel+Music&ei=2lRwZInHEKrnsAf4pKTQCQ&ved=0ahUKEwiJnP7VsJL_AhWqM-wKHXgSCZoQ4dUDCA8&uact=5&oq=Afghan+Nobel+Music&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIFCCEQoAEyBQghEKABOgoIABBHENYEELADSgQIQRgAUMwDWMwDYKsFaANwAXgAgAF0iAF0kgEDMC4xmAEAoAECoAEBwAEByAEI&sclient=gws-wiz-serp")
+req = session.get("Google search address")
 
 soup = BeautifulSoup(req.content, 'html.parser')
 
@@ -14,7 +14,7 @@ for i in soup.find_all('div',{'class':'s75CSd'}):
     keywords.append(i.text)
 
 for w in keywords:
-    session2 = session.get(f'https://www.google.com/search?q={w}&ei=2lRwZInHEKrnsAf4pKTQCQ&ved=0ahUKEwiJnP7VsJL_AhWqM-wKHXgSCZoQ4dUDCA8&uact=5&oq=Afghan+Nobel+Music&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIFCCEQoAEyBQghEKABOgoIABBHENYEELADSgQIQRgAUMwDWMwDYKsFaANwAXgAgAF0iAF0kgEDMC4xmAEAoAECoAEBwAEByAEI&sclient=gws-wiz-serp')
+    session2 = session.get(f'Google search address')
     soup2 = BeautifulSoup(session2.content, 'html.parser')
     for words in soup2.find_all('div',{'class':'s75CSd'}):
         longtail.append(words.text)
